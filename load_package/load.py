867742f8b77d09ff7036538db13c2b2e7daea166
Fabien-LB <errebelneibaf@gmail.com>
2024-05-17 17:18:09 +0200

    correct load_data_csv function to get books by categories

--- load_package/load.py
+++ load_package/load.py
@@ -5,29 +5,36 @@ import csv
 
 # fonction pour créer un fichier csv par catégorie
 # nom du fichier = nom_category.csv
-def load_data_csv(books_data_list):
+def load_data_csv(books_by_category):
+
     # Create the folder Books_Data where all csv files will be saved
     project_path = r"C:\Formation_Dev_Python_OC\Projet_2"
     dl_path = os.path.join(project_path, "Books_Data")
+
+    # Create the Books_Data directory if it doesn't exist
     if not os.path.exists(dl_path):
         os.makedirs(dl_path)
 
-    for book in books_data_list:
-        # csv files will have the name of the value from the key "category"
-        category = book["category"]
+    # Iterate over each category and its associated books
+    for category, books in books_by_category.items():
+
+        # Create a CSV file for each category
         file_path = os.path.join(dl_path, category + ".csv")
-        # create the csv file with header
         with open(file_path, "w", newline="", encoding="utf-8") as csv_file:
+
+            # Define the header for the CSV file
             fieldnames = [
-                "product_page_url", "universal_product_code", "title",
-                "price_including_tax", "price_excludind_tax", "number_avaible",
-                "product_description", "category", "review_rating", "image_url"
+                "product_page_url", "universal_product_code",
+                "title", "price_including_tax", "price_excluding_tax",
+                "number_available", "product_description", "category",
+                "review_rating", "image_url"
             ]
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
-            # iterate in books_data_list to write the data in csv file
-            for book_data in books_data_list:
-                writer.writerow(book_data)
+
+            # Write each book's data to the CSV file
+            for book in books:
+                writer.writerow(book)
 
 
 # fonction pour télécharger les images de livres
