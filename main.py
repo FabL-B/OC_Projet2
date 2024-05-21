da772e21a99088bcb6e7c23c4bb9a713f5d58ca0
Fabien-LB <errebelneibaf@gmail.com>
2024-05-21 18:35:29 +0200

    add input for downloads files path

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from load_package import load
 
 
 def main():
+    # Get the download path
+    download_path = input("Enter the download path: ")
 
-    # Get the book data dictionnary in list
+    # Get the book data dictionary as a list
     books_data_list = transform.get_books_info_dict()
 
-    # Load books data in CSV file sorted by categories
-    load.load_data_csv(books_data_list)
+    books_by_category = transform.sort_books_by_category(books_data_list)
 
-    # Download books pictures with title as filename
-    load.load_img(books_data_list)
+    # Load book data into a CSV file sorted by categories
+    load.load_data_csv(books_by_category, download_path)
+
+    # Download book images with titles as filenames
+    load.load_img(books_data_list, download_path)
 
 
 if __name__ == "__main__":
